d9649fdbd628df2477e70d580050ae439b892632
muharremkulak <kulakmuharrem@gmail.com>
2023-05-17 10:26:42 -0400

    bdd-luma project created using bdd cucumber POM approached!

---
+++ src/main/java/com/academy/techcenture/pages/HomePage.java
@@ -0,0 +1,40 @@
+package com.academy.techcenture.pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage {
+    private WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(this.driver, this);
+    }
+
+    @FindBy(how = How.PARTIAL_LINK_TEXT, using = "Sign In")
+    protected WebElement signInLink;
+
+    @FindBy(how = How.CLASS_NAME, using = "logged-in")
+    protected WebElement signInWelcomeMsg;
+
+    @FindBy(how = How.PARTIAL_LINK_TEXT, using = "Create an Account")
+    protected WebElement createAccountLink;
+
+
+    public void clickSignInLink(){
+        signInLink.click();
+    }
+
+    public void verifyLoggedInMsg(){
+        Assert.assertTrue(signInWelcomeMsg.isDisplayed());
+    }
+
+    public void clickCreateAccountLink(){
+        Assert.assertTrue("Create account link is not displayed", createAccountLink.isDisplayed());
+        createAccountLink.click();
+    }
+}
